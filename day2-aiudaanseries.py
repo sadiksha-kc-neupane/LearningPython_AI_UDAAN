f59d9edabdbe8ec4493ce0410c4f7178767f6ae1
sadiksha-kc-neupane <kcneupanesadiksha@gmail.com>
2026-07-09 00:34:50 +0545

    Initial commit

---
+++ day2-aiudaanseries.py
@@ -0,0 +1,12 @@
+print("Bye World") #string
+print(12345) #number
+print(False) #boolean
+
+bucket = 'water';
+cup = 'coffee';
+age =  12 ;    
+
+
+magic_bucket =['water','coffee','icecream']
+#list
+

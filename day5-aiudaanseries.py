56a4d158ba8e35d08d03c0b1f59f90d4864d63c6
sadiksha-kc-neupane <kcneupanesadiksha@gmail.com>
2026-07-12 23:00:05 +0545

    day5

---
+++ day5-aiudaanseries.py
@@ -0,0 +1,6 @@
+#   Day 5
+
+# Learned about functions in Python, including both built-in functions and user-defined functions.
+
+# Explored lambda functions and how they can be used to write simple functions in a concise way.
+

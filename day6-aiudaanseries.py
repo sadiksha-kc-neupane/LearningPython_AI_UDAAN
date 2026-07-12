75484ae9ab62adf7147302566788268f981c0999
sadiksha-kc-neupane <kcneupanesadiksha@gmail.com>
2026-07-12 23:01:00 +0545

    day6

---
+++ day6-aiudaanseries.py
@@ -0,0 +1,6 @@
+# 📍 Day 6
+
+# Got introduced to the importance of data in Artificial Intelligence and how AI models work with structured data.
+
+# Also learned the basics of NumPy and Pandas, two essential Python libraries for data analysis and manipulation.
+
